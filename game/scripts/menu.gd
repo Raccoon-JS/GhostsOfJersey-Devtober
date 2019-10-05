f146c245d31e13b7374eeaa8953c5058081513ae
Raccoon-JS <raccoon_js@protonmail.com>
2019-10-05 11:53:11 -0500

    Added fonts and the title.

--- game/scripts/menu.gd
+++ game/scripts/menu.gd
@@ -2,6 +2,7 @@ extends Node
 # This is the menu
 
 func _ready():
-	$background/bg_color.color = colors.bi_colors[3]
-	$background/woods.modulate = Color(colors.bi_colors[0])
+	$background/bg_color.color = colors.bi_colors[4]
+	$background/woods.modulate = Color(colors.bi_colors[2])
+	$title.modulate = Color(colors.bi_colors[0])
 	pass # Replace with function body.

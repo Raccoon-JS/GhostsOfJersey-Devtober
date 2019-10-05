0a3e70fd79520ab6fc197e3226816d613a72b1fc
Raccoon-JS <raccoon_js@protonmail.com>
2019-10-05 01:03:41 -0500

    started the project.

---
+++ game/scripts/colors.gd
@@ -0,0 +1,8 @@
+extends Node
+# This is autoload for colors
+
+var trans_colors = ["55CDFC","A3E6E8","FFFFFF","EDC1AB","F7A8B8"]
+var bi_colors = ["FF0080","E27F66","A349A4","3F80A8","0000FF"]
+
+func _ready():
+	pass # Replace with function body.
---
+++ game/scripts/menu.gd
@@ -0,0 +1,7 @@
+extends Node
+# This is the menu
+
+func _ready():
+	$background/bg_color.color = colors.bi_colors[3]
+	$background/woods.modulate = Color(colors.bi_colors[0])
+	pass # Replace with function body.
